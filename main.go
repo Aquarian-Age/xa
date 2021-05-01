616a549406e32e6f554232f2a5efc64827cf3d53
Aquarian-Age <36611556+Aquarian-Age@users.noreply.github.com>
2021-05-01 22:02:32 +0800

    十干禄位

--- main.go
+++ main.go
@@ -14,7 +14,11 @@ func main() {
 	csmap := gz.ChangSheng("甲子") //干对应的长生map
 	fmt.Println(csmap)
 
-	wxsk:=gz.GetWXSKS("癸酉")//五行生克
-	wxn:=gz.Wxsk("癸酉")
-	fmt.Println(wxsk,wxn)
+	wxsk := gz.GetWXSKS("癸酉") //五行生克
+	wxn := gz.Wxsk("癸酉")
+	fmt.Println(wxsk, wxn)
+
+	lu := gz.Lu("癸酉")
+	lu += gz.Lu("庚辰")
+	fmt.Println(lu)
 }
