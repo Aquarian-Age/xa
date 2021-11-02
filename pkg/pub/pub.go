b0d7674b3970cbc1df68da75f134161525491db1
Aquarian-Age <36611556+Aquarian-Age@users.noreply.github.com>
2021-11-03 07:58:33 +0800

    weekName

--- pkg/pub/pub.go
+++ pkg/pub/pub.go
@@ -117,3 +117,24 @@ func SortArrInt(index int, arr []int) ([]int, error) {
 	}
 	return arr, err
 }
+
+//周几 0,7周日　1:周一
+func WeekName(n int) (w string) {
+	switch n {
+	case 0, 7:
+		w = "周日"
+	case 1:
+		w = "周一"
+	case 2:
+		w = "周二"
+	case 3:
+		w = "周三"
+	case 4:
+		w = "周四"
+	case 5:
+		w = "周五"
+	case 6:
+		w = "周六"
+	}
+	return
+}
