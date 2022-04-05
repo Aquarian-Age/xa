c1a3c0e1e7d983b75cd1828930f2cf7a5d8e91c2
Aquarian-Age <36611556+Aquarian-Age@users.noreply.github.com>
2022-04-05 15:09:48 +0800

    贵人

--- pkg/gz/贵人诀.go
+++ pkg/gz/贵人诀.go
@@ -6,54 +6,44 @@
 
 package gz
 
-import "strings"
+import (
+	"github.com/Aquarian-Age/xa/pkg/pub"
+)
 
 //阴阳贵人诀 返回干支对应的阳贵人　阴贵人
 func GuiRenJue(dgz string) (yang, yin string) {
-	yiJi := []string{"申", "子"}   //乙申阳　乙子阴　己申阴　己子阳
-	jianXu := []string{"未", "丑"} //甲未阳　甲丑阴　戊未阴　戊丑阳
-	bingDing := []string{"酉", "亥"}
-	renGui := []string{"卯", "巳"}
-	geng := []string{"丑", "未"}
-	xin := []string{"寅", "午"}
-	gan := []string{"甲", "乙", "丙", "丁", "戊", "己", "geng", "xin", "壬", "癸"}
-	var i int
-	for i = 0; i < len(gan); i++ {
-		if strings.ContainsAny(dgz, gan[i]) {
-			break
-		}
-	}
-	switch i {
-	case 0: //甲
-		yang = jianXu[0]
-		yin = jianXu[1]
-	case 4: //戊
-		yang = jianXu[1]
-		yin = jianXu[0]
-	case 1: //乙
-		yang = yiJi[0]
-		yin = yiJi[1]
-	case 5: //己
-		yang = yiJi[1]
-		yin = yiJi[0]
-	case 2: //丙
-		yang = bingDing[0]
-		yin = bingDing[1]
-	case 3: //丁
-		yang = bingDing[1]
-		yin = bingDing[0]
-	case 6: //geng
-		yang = geng[0]
-		yin = geng[1]
-	case 7: //xin
-		yang = xin[0]
-		yin = xin[1]
-	case 8: //壬
-		yang = renGui[0]
-		yin = renGui[1]
-	case 9: //癸
-		yang = renGui[1]
-		yin = renGui[0]
+	gs := pub.GetGanS(dgz)
+	switch gs {
+	case "甲":
+		yang = "未"
+		yin = "丑"
+	case "戊":
+		yang = "丑"
+		yin = "未"
+	case "乙":
+		yang = "申"
+		yin = "子"
+	case "己":
+		yang = "子"
+		yin = "申"
+	case "丙":
+		yang = "酉"
+		yin = "亥"
+	case "丁":
+		yang = "亥"
+		yin = "酉"
+	case "庚":
+		yang = "丑"
+		yin = "未"
+	case "辛":
+		yang = "寅"
+		yin = "午"
+	case "壬":
+		yang = "卯"
+		yin = "巳"
+	case "癸":
+		yang = "巳"
+		yin = "卯"
 	}
 	return
 }
